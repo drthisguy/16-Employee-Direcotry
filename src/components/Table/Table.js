946013ee0ce1241837e9eb279ae8fdbcd99d1320
My Lord <page.c.tyler@gmail.com>
2020-04-09 10:20:12 -0400

    Table component dried up and converted to a functional component.

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,62 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 // import PropTypes from 'prop-types'
 
 
-export default class Table extends Component {
-    
-    constructor(props){
-      super(props);
-      this.getHeader = this.getHeader.bind(this);
-      this.getRowData = this.getRowData.bind(this);
-      this.getColumns = this.getColumns.bind(this);
+export default function Table(props) {
+        
+    const getHeader = () => {
+      const columns = ['Photo', 'Name', 'Email', 'Phone', 'Age',]
+      return columns.map( column => <th>{column}</th>)
     }
     
-    getColumns = function(){
-        return ['Photo', 'Name', 'Email', 'Phone', 'DOB',]
- 
+    const getRows = () => {
+      const items = props.data;
+      return items.map( row => <tr><RenderRow data={row}/></tr>)
     }
     
-    getHeader = function(){
-      const columns = this.getColumns()
-      return columns.map( column => <th column={column}>{column}</th>)
-    }
-    
-    getRowData = function(){
-      const items = this.props.data;
-      console.log("Table -> getRowData -> items", items)
-      const keys = this.getColumns();
-      return items.map((row, index)=>{
-        return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
-      })
-    }
-    
-    render() {
-        return (
-          <div>
-            <table>
-            <thead>
-              <tr>{this.getHeader()}</tr>
-            </thead>
-            <tbody>
-              {this.getRowData()}
-            </tbody>
-            </table>
-          </div>
-          
-        );
-    }
+    return (
+        <div>
+        <table>
+        <thead>
+            <tr>{getHeader()}</tr>
+        </thead>
+        <tbody>
+            {getRows()}
+        </tbody>
+        </table>
+        </div>
+        
+    );
 }
 
 function RenderRow(props) {
-  
     return (
         <React.Fragment>
-            <td key={props.data.avatar}>{props.data.avatar}</td>
-            <td key={props.data.name.last}>{props.data.name.first}{' '}{props.data.name.last}</td>
-            <td key={props.data.phone}>{props.data.phone}</td>
-            <td key={props.data.email}>{props.data.email}</td>
-            <td key={props.data.dob.age}>{props.data.dob.age}</td>
+            <td>{props.data.avatar}</td>
+            <td>{props.data.name.first}{' '}{props.data.name.last}</td>
+            <td>{props.data.phone}</td>
+            <td>{props.data.email}</td>
+            <td>{props.data.dob.age}</td>
         </React.Fragment>
     );
- 
 }
