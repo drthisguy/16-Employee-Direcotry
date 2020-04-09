b23a6d9f83519a1029b1f6045e69e46756290801
My Lord <page.c.tyler@gmail.com>
2020-04-09 04:32:06 -0400

    search field componenent built.

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./style.css";   
+
+export default function Navbar() {
+    return (
+        <header className="header">
+            <h1>Employee Directory</h1>
+        </header>
+    )
+}
---
+++ src/components/Search/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types'
+
+export class SearchField extends Component {
+    state = {
+        query: ""
+    }
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
+          <input
+            style={{ flex: "10", padding: "5px" }}
+            type="text"
+            name="query"
+            placeholder="Search Directory ..."
+            value={this.state.query}
+            onChange={this.onChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+// propTypes
+SearchField.propTypes = {
+  SearchField: PropTypes.func.isRequired
+}
+
+
+export default SearchField;
